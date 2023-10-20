83ed259d27a369ad2cba3a787a467c444bd18667
damar <damar@gitlab.lab>
2023-10-20 13:42:44 +0700

    transfer opt and picks opt

--- fpl-data.js
+++ fpl-data.js
@@ -8,7 +8,33 @@ const playerData = new Promise((resolve, reject) => {
         responseType: 'application/json'
       })
     .then(function (response) {
-        resolve((JSON.parse(response.data)).elements)
+        resolve((JSON.parse(response.data)))
+    });
+})
+
+const fixturesData = new Promise((resolve, reject) => {
+    const axios = require('axios');
+    
+    axios({
+        method: 'get',
+        url: 'https://fantasy.premierleague.com/api/fixtures/',
+        responseType: 'application/json'
+      })
+    .then(function (response) {
+        resolve((JSON.parse(response.data)))
+    });
+})
+
+const picksData = (id, gameWeek) => new Promise((resolve, reject) => {
+    const axios = require('axios');
+    
+    axios({
+        method: 'get',
+        url: `https://fantasy.premierleague.com/api/entry/${id}/event/${gameWeek}/picks/`,
+        responseType: 'application/json'
+      })
+    .then(function (response) {
+        resolve((JSON.parse(response.data)))
     });
 })
 
@@ -55,7 +81,81 @@ const getExpectedPoints = (element, gameWeek) => {
     return xP;
 }
 
+const getTotalXPMultiplies = (bootstrap, gameWeek, deltaGW, picksData, fixtures) => {
+    const currentFixtures = fixtures.filter(data => data.event === gameWeek + deltaGW);
+    const haIndexData = []
+    const { elements, teams } = bootstrap;
+    for (let f of currentFixtures) {
+        const data = {
+            home: f.team_h,
+            away: f.team_a,
+            homeOff: teams.find(t => t.id === f.team_h).strength_attack_home,
+            homeDef: teams.find(t => t.id === f.team_h).strength_defence_home,
+            awayOff: teams.find(t => t.id === f.team_a).strength_attack_away,
+            awayDef: teams.find(t => t.id === f.team_a).strength_defence_away,
+            homeDiff: f.team_h_difficulty,
+            awayDiff: f.team_a_difficulty
+        }
+
+        haIndexData.push(data);
+        // console.log(teams.find(t => t.id === f.team_h).short_name, ' v ', teams.find(t => t.id === f.team_a).short_name);
+    }
+
+    const myTeam = []
+    let totalXPoints = 0;
+    for (let pick of picksData.picks) {
+        let xPPerElement = 0;
+        if (elements.find((o) => pick.element === o.id)) {
+            const dataEl = elements.find((o) => pick.element === o.id);
+
+            const xP = getExpectedPoints(dataEl, gameWeek);
+            const home = haIndexData.filter(ha => ha.home === dataEl.team);
+            const away = haIndexData.filter(ha => ha.away === dataEl.team);
+
+            let haIdxValue = 1;
+            if (home && home.length > 0) {
+                for (let h of home) {
+                    if (dataEl.element_type === 4) {
+                        haIdxValue = ((1 * h.homeOff / h.awayDef) + (0 * h.homeDef / h.awayOff)) // * (4 / h.awayDiff);
+                    } else if (dataEl.element_type === 3) {
+                        haIdxValue = (((8/9) * h.homeOff / h.awayDef) + ((1/9) * h.homeDef / h.awayOff)) // * (4 / h.awayDiff);
+                    } else if (dataEl.element_type === 2) {
+                        haIdxValue = (((9/15) * h.homeOff / h.awayDef) + ((6/15) * h.homeDef / h.awayOff)) // * (4 / h.awayDiff);
+                    } else if (dataEl.element_type === 1) {
+                        haIdxValue = ((0 * h.homeOff / h.awayDef) + (1 * h.homeDef / h.awayOff)) // * (4 / h.awayDiff);
+                    }
+                    totalXPoints += xP * pick.multiplier * haIdxValue;
+                    xPPerElement += xP * pick.multiplier * haIdxValue;
+                }
+            } 
+            
+            if (away && away.length > 0) {
+                for (let a of away) {
+                    if (dataEl.element_type === 4) {
+                        haIdxValue = ((1 * a.homeOff / a.awayDef) + (0 * a.homeDef / a.awayOff)) // * (4 / a.homeDiff);
+                    } else if (dataEl.element_type === 3) {
+                        haIdxValue = (((8/9) * a.homeOff / a.awayDef) + ((1/9) * a.homeDef / a.awayOff)) // * (4 / a.homeDiff);
+                    } else if (dataEl.element_type === 2) {
+                        haIdxValue = (((9/15) * a.homeOff / a.awayDef) + ((6/15) * a.homeDef / a.awayOff)) // * (4 / a.homeDiff);
+                    } else if (dataEl.element_type === 1) {
+                        haIdxValue = ((0 * a.homeOff / a.awayDef) + (1 * a.homeDef / a.awayOff)) // * (4 / a.homeDiff);
+                    }
+                }
+                totalXPoints += xP * pick.multiplier * haIdxValue;
+                xPPerElement += xP * pick.multiplier * haIdxValue;
+            }
+
+            myTeam.push({...dataEl, multiplier: pick.multiplier, xPFinal: xPPerElement})
+
+        }
+    }   
+    return {myTeam, totalXPoints};
+}
+
 module.exports = {
     playerData,
-    getExpectedPoints
+    fixturesData,
+    picksData,
+    getExpectedPoints,
+    getTotalXPMultiplies
 }
--- index.js
+++ index.js
@@ -1,42 +1,57 @@
+/**
+ * hold players
+ */
+const mandatoryPlayer = [
+    'Areola',
+    'Leno',
+    'Pedro Porro',
+    'Haaland',
+    'Maddison',
+    // 'Burn',
+    'Salah',
+    'Son',
+    // 'Neto',
+    'Watkins',
+    // 'Lamptey',
+    // 'J.Alvarez'
+]
+/**
+ * fpl money
+ */
+const money = 101.5
+
+
 const fplData = require('./fpl-data')
 
-fplData.playerData.then((elements) => {
+const playerData = fplData.playerData;
+const fixturesData = fplData.fixturesData;
 
+Promise.all([playerData, fixturesData]).then(([{elements, events, teams}, fixtures]) => {
     /**
-     * hold players
+     * create variable models
+     * @param {string} suffix 
+     * @param {function} filterCat 
+     * @param {Array} addEntries 
+     * @returns 
      */
-    const mandatoryPlayer = [
-        'Areola',
-        'Leno',
-        'Pedro Porro',
-        'Haaland',
-        'Maddison',
-        'Burn',
-        'Salah',
-        'Son',
-        'Neto',
-        'Watkins',
-        'Lamptey',
-        'J.Alvarez'
-    ]
-    /**
-     * fpl money * 10
-     */
-    const money = 1015
-    const fplVariables = Object.fromEntries(elements.map(e => {
+    const createVariables = (suffix, filterCat, addEntries) => Object.fromEntries(elements.map(e => {
+        const picksData = { picks: [
+            {
+                element: parseInt(e.id),
+                multiplier: 1
+            }
+        ]}
+        const gameWeek = events.find(o => o.is_current).id;
+        
         let entries = Object.fromEntries([
-            [e.web_name, 1],
-            [`captain_${e.web_name}`, 1],
-            [`bench_${e.web_name}`, 1],
+            [`${e.web_name}${suffix}`, 1],
+            ...addEntries,
             ['fwd', e.element_type == 4 ? 1 : 0],
             ['mid', e.element_type == 3 ? 1 : 0],
             ['def', e.element_type == 2 ? 1 : 0],
             ['gkp', e.element_type == 1 ? 1 : 0],
-            ['xp', fplData.getExpectedPoints(e, 8)],
+            ['xp', fplData.getTotalXPMultiplies({elements, teams}, gameWeek, 1, picksData, fixtures).totalXPoints],
             [`team_${e.team_code}`, 1],
-            ['point_pick', fplData.getExpectedPoints(e, 8)],
-            ['point_captain', fplData.getExpectedPoints(e, 8) * 2],
-            ['point_bench', 0]
         ]);
 
         
@@ -45,11 +60,20 @@ fplData.playerData.then((elements) => {
             max_pick: 1, 
             ...entries,
         }})
-            .map(e => [e.web_name, e]));
+            .filter(filterCat)
+            .map(e => [`${e.web_name}${suffix}`, e]));
 
-    // only integers
-    const fplInts = Object.fromEntries(elements.map(e => [e.web_name, 1]))
-    
+    /**
+     * 
+     * 
+     * transfer opt
+     * 
+     */
+
+    // variables
+    const fplVariables = createVariables('', (v) => { return v }, []);
+
+    // constraints
     const maxPick = Object.fromEntries(elements.map(e => [e.web_name, {"max": 1, "min": 0}]));
     const posConstraints = {
         "gkp": {"equal": 2},
@@ -57,27 +81,23 @@ fplData.playerData.then((elements) => {
         "mid": {"equal" : 5},
         "fwd": {"equal": 3}
     };
-    
-
     const playerConstraints = Object.fromEntries(mandatoryPlayer.map(p => [p, {"equal": 1}]))
     const teamConstaints = Object.fromEntries(elements.map(e => [`team_${e.team_code}`, {"max": 3}]))
-    const pickConstaints = Object.fromEntries(elements.map(e => 
-        [
-            [`captain_${e.team_code}`, {"max": 1}],
-            [`bench_${e.team_code}`, {"equal": 4}],
-        ]
-    ))
+    
+    // only integers
+    const fplInts = Object.fromEntries(elements.map(e => [e.web_name, 1]))
+    
     var solver = require("javascript-lp-solver/src/solver"),
+    // transfer optimization model
     model = {
         "optimize": "xp",
         "opType": "max",
         "constraints": {
             ...maxPick,
-            "now_cost": {"max": money},
+            "now_cost": {"max": money * 10},
             ...posConstraints,
             ...playerConstraints,
-            ...teamConstaints,
-            ...pickConstaints
+            ...teamConstaints
         },
         "variables": {
             ...fplVariables
@@ -86,9 +106,69 @@ fplData.playerData.then((elements) => {
             ...fplInts
         }
     }
+    console.log('--- tranf opt ---')
     const solution = solver.Solve(model);
     console.table(solution);
     
+
+    /**
+     * 
+     * 
+     * pick opt
+     * 
+     * 
+     */
+
+    // variables
+    const fplVariables2 = createVariables('', (v) => Object.keys(solution).filter(a => a != 'feasible' && a != 'result' && a!= 'bounded' && a != 'isIntegral').includes(v.web_name), [])
+    const fplCaptaincyVariables2 = createVariables('*', (v) => Object.keys(solution).filter(a => a != 'feasible' && a != 'result' && a!= 'bounded' && a != 'isIntegral').includes(v.web_name), [[`capt_check`, 1],])
+    
+    // constraints
+    const maxPick2 = Object.fromEntries(elements.map(e => [e.web_name, {"max": 1, "min": 0}]));
+    const posConstraints2 = {
+        "gkp": {"min": 1, "max": 2},
+        "def": {"min": 3, "max": 6},
+        "mid": {"min": 2, "max": 6},
+        "fwd": {"min": 1, "max": 4}
+    };
+    const playerConstraints2 = Object.fromEntries(mandatoryPlayer.map(p => [p, {"min": 0, "max": 1}]))
+    const captaincyConstraints = Object.fromEntries(elements.map(e => [`capt_check`, {"max": 1}]))
+    
+    // pick optimization model
+    model = {
+        "optimize": "xp",
+        "opType": "max",
+        "constraints": {
+            ...maxPick2,
+            // "now_cost": {"max": money},
+            ...posConstraints2,
+            ...playerConstraints2,
+            ...teamConstaints,
+            ...captaincyConstraints,
+            "max_pick": {"max": 12}
+        },
+        "variables": {
+            ...fplVariables2,
+            ...fplCaptaincyVariables2
+        },
+        "ints": {
+            ...fplInts
+        }
+    }
+
+    console.log('--- pick opt ---')
+    const solution2 = solver.Solve(model);
+    console.table(solution2);
+
+
+    /**
+     * 
+     * 
+     * cost
+     * 
+     * 
+     */
+
     let cost = 0;
     const players = Object.keys(solution).filter(a => a != 'feasible' && a != 'result' && a!= 'bounded' && a != 'isIntegral');
     for (let p of players) {
@@ -96,5 +176,6 @@ fplData.playerData.then((elements) => {
         cost += eee.now_cost;
     }
     console.log('cost:', cost/10);
+
 })
 
